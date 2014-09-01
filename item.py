56fd47c42c47c1e12785779e5d5449653b968a0a
Travis Dean <travis.j.dean@gmail.com>
2014-09-01 13:17:45 -0400

    Code cleanup. Added exception catching.

--- item.py
+++ item.py
@@ -1,15 +1,17 @@
 import math
 __author__ = 'Travis Dean'
 
+
 class Item:
     """Keeps track of the dataset items."""
-    def __init__(self, x, y, category = "None"):
+    def __init__(self, x, y, category="None"):
         self.x = float(x)
         self.y = float(y)
         self.category = category
 
     def distance(self, other):
-        return math.sqrt(math.pow((self.x-other.x),2) + math.pow((self.y - other.y),2))
+        return math.sqrt(math.pow((self.x-other.x), 2)
+                         + math.pow((self.y - other.y), 2))
 
     # def distance(self, x, y):
     #     return math.sqrt(math.pow((self.x-x),2) + math.pow((self.y - y),2))
--- knn.py
+++ knn.py
@@ -3,34 +3,40 @@
 from item import Item
 from collections import Counter
 __author__ = 'Travis Dean'
-debug = False;
+debug = False
+
 
 def prompt():
     k = int(raw_input("Enter the value of k: "))
     m = int(raw_input("Enter the number of values to be read: "))
     filename = raw_input("Data file name: ")
     print("When entering value pairs, separate by spaces without commas.")
-    return (k,m,filename)
+    return k, m, filename
+
 
 def get_input(k, m, filename):
     data = read_data(filename)[:m]
     while True:
-        x,y = raw_input("Enter a (X,Y) pair: ").split()
-        if (x == '1.0' and y == '1.0'): break
+        x, y = raw_input("Enter a (X,Y) pair: ").split()
+        if x == '1.0' and y == '1.0': break
         process_input(data, k, float(x), float(y))
 
+
 def read_data(filename):
-    lines = open(filename).readlines()
+    # lines = open(filename).readlines()
+    with open(filename) as f:
+        lines = f.readlines()
     data = []
     for d in lines:
-        c,x,y = d.split()
-        data.append(Item(x,y,c))
+        c, x, y = d.split()
+        data.append(Item(x, y, c))
     return data
+
 
 def process_input(data, k, x, y):
     count = Counter()
     totals = Counter()
-    d = Item(x,y)
+    d = Item(x, y)
     nearest = sorted(data, key=lambda i: i.distance(d))[:k]
     for n in nearest:
         totals[n.category] += n.distance(d)
@@ -39,15 +45,20 @@ def process_input(data, k, x, y):
 
     winner = count.most_common(1)
     win_cat = winner[0][0]
-    print("Data item (%s,%s) assigned to: %s" % (x,y,win_cat))
+    print("Data item (%s,%s) assigned to: %s" % (x, y, win_cat))
 
     for cat, total in totals.items():
-        print("Average distance to %s items: %f" % (cat, total / count[cat]))
+        print("Average distance to %s items: %f" %
+              (cat, total / count[cat]))
+
 
 if __name__ == "__main__":
     if not debug:
-        get_input(*prompt())
+        try:
+            get_input(*prompt())
+        except Exception as e:
+            print('Invalid input: ' + str(e))
     else:
-        get_input(3,8, "testdata.txt")
+        get_input(3, 8, "testdata.txt")
 
 
